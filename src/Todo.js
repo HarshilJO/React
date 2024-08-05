f80b9f23417c377c6ba81e6a9d9d30ab1fc2e87e
harshil <harshil.y.joshi.2004@gmail.com>
2024-08-05 20:14:25 +0530

    style updated

--- src/Todo.js
+++ src/Todo.js
@@ -2,11 +2,33 @@ import React from 'react';
 const Todo=({ todo, index, toggleTodo, deleteTodo })=>{
     return ( 
         <>
-            <div className="task" id="checklist">
-                <input value="2" name="r" type="checkbox" id="02" onClick={() => toggleTodo(index)} />
-                <label htmlFor="02">{todo.text}.</label>
-                <button type="button" className="dismiss" onClick={() => deleteTodo(index)}>×</button>
-            </div>
+           <div className="task" id="checklist">
+    <input 
+        value="2" 
+        name="r" 
+        type="checkbox" 
+        id="02" 
+        onClick={() => toggleTodo(index)} 
+    />
+    <label 
+        htmlFor="02" 
+        style={{
+            wordWrap: 'break-word',
+            overflowWrap: 'break-word',
+            whiteSpace: 'normal',
+            flex: '1'
+        }}
+    >
+        {todo.text}
+    </label>
+    <button 
+        type="button" 
+        className="dismiss" 
+        onClick={() => deleteTodo(index)}
+    >x
+    </button>
+</div>
+
         </>
      );
 }
